12948e3b0ae149b37681548512e3144c2d91a178
Ludovic Loridan <ludovic.loridan@gmail.com>
2015-03-03 16:19:21 +0100

    Updated Critizr.framework

---
+++ sample/Critizr.framework/Versions/A/Headers/Critizr.h
@@ -0,0 +1,11 @@
+//
+//  Critizr.h
+//  Critizr
+//
+//  Created by Ludovic Loridan on 2/25/15.
+//  Copyright (c) 2015 Critizr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <Critizr/CRFeedbackDialog.h>
